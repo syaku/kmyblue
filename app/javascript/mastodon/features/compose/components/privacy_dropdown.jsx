1c2eb1fc6d9704423592955d9a757720b36eea8c
KMY <tt@kmycode.net>
2023-03-05 17:50:41 +0900

    Add public_unlisted translation

--- app/javascript/mastodon/features/compose/components/privacy_dropdown.jsx
+++ app/javascript/mastodon/features/compose/components/privacy_dropdown.jsx
@@ -13,7 +13,7 @@ const messages = defineMessages({
   unlisted_short: { id: 'privacy.unlisted.short', defaultMessage: 'Unlisted' },
   unlisted_long: { id: 'privacy.unlisted.long', defaultMessage: 'Visible for all, but opted-out of discovery features' },
   public_unlisted_short: { id: 'privacy.public_unlisted.short', defaultMessage: 'Public unlisted' },
-  public_unlisted_long: { id: 'privacy.public_unlisted.long', defaultMessage: 'Visible for all, but dont visible in GTL' },
+  public_unlisted_long: { id: 'privacy.public_unlisted.long', defaultMessage: 'Visible for all without GTL' },
   private_short: { id: 'privacy.private.short', defaultMessage: 'Followers only' },
   private_long: { id: 'privacy.private.long', defaultMessage: 'Visible for followers only' },
   direct_short: { id: 'privacy.direct.short', defaultMessage: 'Mentioned people only' },
