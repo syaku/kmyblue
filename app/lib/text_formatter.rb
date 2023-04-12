71917cab04ddb636440f70714acd48263bb316bb
KMY <tt@kmycode.net>
2023-04-12 17:06:27 +0900

    Fix no-markdown post line break

--- app/lib/text_formatter.rb
+++ app/lib/text_formatter.rb
@@ -47,7 +47,7 @@ class TextFormatter
     # line first letter for blockquote
     html = markdownify(html.gsub(/^&gt;/, '>')) if markdown?
 
-    # html = simple_format(html, {}, sanitize: false).delete("\n") if multiline?
+    html = simple_format(html, {}, sanitize: false).delete("\n") if !markdown? && multiline?
     html = html.delete("\n")
 
     html.html_safe # rubocop:disable Rails/OutputSafety
