fac734b6a88a9079692b46c00ac32a39030a7141
KMY <tt@kmycode.net>
2023-04-13 18:55:36 +0900

    Add emoji_reactions_count on status

--- app/serializers/rest/status_serializer.rb
+++ app/serializers/rest/status_serializer.rb
@@ -6,7 +6,7 @@ class REST::StatusSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :in_reply_to_id, :in_reply_to_account_id,
              :sensitive, :spoiler_text, :visibility, :visibility_ex, :language, :translatable,
              :uri, :url, :replies_count, :reblogs_count, :searchability, :markdown,
-             :favourites_count, :emoji_reactions, :edited_at
+             :favourites_count, :emoji_reactions, :emoji_reactions_count, :edited_at
 
   attribute :favourited, if: :current_user?
   attribute :reblogged, if: :current_user?
