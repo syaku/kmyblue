50118163f754bdedffd3c4e302dfbb69f9d4a6ce
KMY <tt@kmycode.net>
2023-05-02 20:21:35 +0900

    Fix antenna sql

--- app/services/fan_out_on_write_service.rb
+++ app/services/fan_out_on_write_service.rb
@@ -122,12 +122,17 @@ class FanOutOnWriteService < BaseService
     domain = @account.domain || Rails.configuration.x.local_domain
 
     antennas = Antenna.availables
-    antennas = antennas.left_joins(:antenna_accounts).where(any_accounts: true).or(Antenna.availables.left_joins(:antenna_accounts)                                                       .where(antenna_accounts: { exclude: false, account: @account }))
-    antennas = antennas.left_joins(:antenna_domains) .where(any_domains: true) .or(Antenna.availables.left_joins(:antenna_accounts).left_joins(:antenna_domains)                          .where(antenna_domains:  { exclude: false, name: domain }))
-    antennas = antennas.left_joins(:antenna_tags)    .where(any_tags: true)    .or(Antenna.availables.left_joins(:antenna_accounts).left_joins(:antenna_domains).left_joins(:antenna_tags).where(antenna_tags:     { exclude: false, tag: @status.tags }))
-    antennas = antennas.where(account: @account.followers) if @status.visibility.to_sym == :unlisted
+    antennas = antennas.left_joins(:antenna_domains).where(any_domains: true).or(Antenna.left_joins(:antenna_domains).where(antenna_domains: { name: domain }))
     antennas = antennas.where(with_media_only: false) if !@status.with_media?
     antennas = antennas.where.not(account: @account.blocking)
+
+    antennas = Antenna.where(id: antennas.select(:id))
+    antennas = antennas.left_joins(:antenna_accounts).where(any_accounts: true).or(Antenna.left_joins(:antenna_accounts).where(antenna_accounts: { account: @account }))
+
+    tag_ids = @status.tags.pluck(:id)
+    antennas = Antenna.where(id: antennas.select(:id))
+    antennas = antennas.left_joins(:antenna_tags).where(any_tags: true).or(Antenna.left_joins(:antenna_tags).where(antenna_tags: { tag_id: tag_ids }))
+
     antennas.in_batches do |ans|
       ans.each do |antenna|
         next if !antenna.enabled?
