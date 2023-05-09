2a1a2da06a7ed91f36639548c762cefd5468143e
KMY <tt@kmycode.net>
2023-05-09 18:41:02 +0900

    Fix antenna validation

--- app/models/antenna.rb
+++ app/models/antenna.rb
@@ -42,7 +42,7 @@ class Antenna < ApplicationRecord
   validate :list_owner
 
   def list_owner
-    raise Mastodon::ValidationError, I18n.t('antennas.errors.invalid_list_owner') if list.present? && list.account != account
+    raise Mastodon::ValidationError, I18n.t('antennas.errors.invalid_list_owner') if !list_id.zero? && list.present? && list.account != account
   end
 
   def enabled?
