ce4e076570b587980cc407a1e1b43b4257a16e83
KMY <tt@kmycode.net>
2023-05-17 20:21:17 +0900

    Remove unnecessary index

--- db/migrate/20230512122757_add_reject_reply_exclude_followers_to_domain_blocks.rb
+++ db/migrate/20230512122757_add_reject_reply_exclude_followers_to_domain_blocks.rb
@@ -3,6 +3,5 @@
 class AddRejectReplyExcludeFollowersToDomainBlocks < ActiveRecord::Migration[6.1]
   def change
     add_column :domain_blocks, :reject_reply_exclude_followers, :boolean, null: false, default: false
-    add_index :domain_blocks, :reject_reply_exclude_followers
   end
 end
