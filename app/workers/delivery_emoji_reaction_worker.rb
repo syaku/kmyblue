dba56200520f34ceda0fd17d68897818f279a3c5
KMY <tt@kmycode.net>
2023-05-29 20:02:38 +0900

    Fix emoji reaction notification

--- app/workers/delivery_emoji_reaction_worker.rb
+++ app/workers/delivery_emoji_reaction_worker.rb
@@ -6,19 +6,12 @@ class DeliveryEmojiReactionWorker
   include Lockable
   include AccountScope
 
-  def perform(payload_json, status_id, my_account_id = nil)
-    redis.publish("timeline:#{my_account_id}", payload_json) if my_account_id.present? && !Account.find(my_account_id)&.user&.setting_stop_emoji_reaction_streaming
-
+  def perform(payload_json, status_id, _my_account_id = nil)
     status = Status.find(status_id.to_i)
 
     if status.present?
       scope_status(status).includes(:user).find_each do |account|
         redis.publish("timeline:#{account.id}", payload_json) if !account.user&.setting_stop_emoji_reaction_streaming && redis.exists?("subscribed:timeline:#{account.id}")
-      end
-
-      if [:public, :unlisted, :public_unlisted, :login].exclude?(status.visibility.to_sym) && status.account_id != my_account_id &&
-         redis.exists?("subscribed:timeline:#{status.account_id}") && !status.account.user&.setting_stop_emoji_reaction_streaming
-        redis.publish("timeline:#{status.account_id}", payload_json)
       end
     end
 
