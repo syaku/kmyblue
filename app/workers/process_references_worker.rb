93ffe2cff2bb41a25195be46996d407380a4d1d2
KMY <tt@kmycode.net>
2023-09-27 13:40:36 +0900

    Add sidekiq option to process references worker

--- app/workers/process_references_worker.rb
+++ app/workers/process_references_worker.rb
@@ -3,6 +3,8 @@
 class ProcessReferencesWorker
   include Sidekiq::Worker
 
+  sidekiq_options queue: 'pull', retry: 3
+
   def perform(status_id, ids, urls, no_fetch_urls = nil)
     ProcessReferencesService.new.call(Status.find(status_id), ids || [], urls: urls || [], no_fetch_urls: no_fetch_urls)
   rescue ActiveRecord::RecordNotFound
