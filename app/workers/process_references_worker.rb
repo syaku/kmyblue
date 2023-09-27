f9ccc886573134193855c3f70d827fe403f484e1
KMY <tt@kmycode.net>
2023-09-27 11:27:52 +0900

    Fix test

--- app/workers/process_references_worker.rb
+++ app/workers/process_references_worker.rb
@@ -3,7 +3,7 @@
 class ProcessReferencesWorker
   include Sidekiq::Worker
 
-  def perform(status_id, ids, urls, no_fetch_urls)
+  def perform(status_id, ids, urls, no_fetch_urls = nil)
     ProcessReferencesService.new.call(Status.find(status_id), ids || [], urls: urls || [], no_fetch_urls: no_fetch_urls)
   rescue ActiveRecord::RecordNotFound
     true
