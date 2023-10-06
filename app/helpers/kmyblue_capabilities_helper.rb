f95a995b81ba270ff534064cbfc1c7f0bda8a4b3
KMY <tt@kmycode.net>
2023-10-06 19:00:19 +0900

    Capabilitiesの検索許可にローカルを追加

--- app/helpers/kmyblue_capabilities_helper.rb
+++ app/helpers/kmyblue_capabilities_helper.rb
@@ -17,6 +17,7 @@ module KmyblueCapabilitiesHelper
       :kmyblue_bookmark_category,
       :kmyblue_quote,
       :kmyblue_searchability_limited,
+      :kmyblue_searchability_public_unlisted,
     ]
 
     capabilities << :profile_search unless Chewy.enabled?
