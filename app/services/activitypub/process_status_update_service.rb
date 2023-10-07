4a6b0062b28405606b10057506fc8b8e211d51b8
KMY <tt@kmycode.net>
2023-10-07 14:10:19 +0900

    カスタム絵文字のライセンス変更を他の場所でも保存

--- app/services/activitypub/process_status_update_service.rb
+++ app/services/activitypub/process_status_update_service.rb
@@ -239,11 +239,16 @@ class ActivityPub::ProcessStatusUpdateService < BaseService
 
       emoji = CustomEmoji.find_by(shortcode: custom_emoji_parser.shortcode, domain: @account.domain)
 
-      next unless emoji.nil? || custom_emoji_parser.image_remote_url != emoji.image_remote_url || (custom_emoji_parser.updated_at && custom_emoji_parser.updated_at >= emoji.updated_at)
+      next unless emoji.nil? ||
+                  custom_emoji_parser.image_remote_url != emoji.image_remote_url ||
+                  (custom_emoji_parser.updated_at && custom_emoji_parser.updated_at >= emoji.updated_at) ||
+                  custom_emoji_parser.license != emoji.license
 
       begin
-        emoji ||= CustomEmoji.new(domain: @account.domain, shortcode: custom_emoji_parser.shortcode, uri: custom_emoji_parser.uri, is_sensitive: custom_emoji_parser.is_sensitive, license: custom_emoji_parser.license)
+        emoji ||= CustomEmoji.new(domain: @account.domain, shortcode: custom_emoji_parser.shortcode, uri: custom_emoji_parser.uri)
         emoji.image_remote_url = custom_emoji_parser.image_remote_url
+        emoji.license = custom_emoji_parser.license
+        emoji.is_sensitive = custom_emoji_parser.is_sensitive
         emoji.save
       rescue Seahorse::Client::NetworkingError => e
         Rails.logger.warn "Error storing emoji: #{e}"
