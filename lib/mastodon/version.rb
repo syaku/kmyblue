3a1b03e2b4af123fa4a0f1bb70b3edae66969b42
KMY <tt@kmycode.net>
2023-10-08 20:18:33 +0900

    Bump version to 5.4 LTS

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -9,7 +9,7 @@ module Mastodon
     end
 
     def kmyblue_minor
-      3
+      4
     end
 
     def kmyblue_flag
@@ -29,7 +29,7 @@ module Mastodon
     end
 
     def default_prerelease
-      'rc1'
+      ''
     end
 
     def prerelease
