1213d5fa200ff00fd04a9b85b84cab32caa86fc1
KMY（雪あすか） <tt@kmycode.net>
2023-10-21 10:38:14 +0900

    Fix: #137 ローカルのカスタム絵文字に`remote_url`が設定されるバグを修正したのに伴う、既存のデータから削除するマイグレーションコード (#158)

---
+++ db/post_migrate/20231021005339_remove_remote_uri_from_local_custom_emojis.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require Rails.root.join('lib', 'mastodon', 'migration_helpers')
+
+class RemoveRemoteUriFromLocalCustomEmojis < ActiveRecord::Migration[7.0]
+  include Mastodon::MigrationHelpers
+
+  disable_ddl_transaction!
+
+  class CustomEmoji < ApplicationRecord; end
+
+  def up
+    safety_assured do
+      CustomEmoji.transaction do
+        CustomEmoji.where(domain: nil).update_all(image_remote_url: nil, uri: nil) # rubocop:disable Rails/SkipsModelValidations
+      end
+    end
+  end
+
+  def down; end
+end
