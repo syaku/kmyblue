2ab2e0f6e205a739a95b1dcd1b17f41b8410de6c
KMY <tt@kmycode.net>
2024-03-07 08:25:22 +0900

    Revertしたコミットに注意書きを追加

--- app/models/account_suggestions/similar_profiles_source.rb
+++ app/models/account_suggestions/similar_profiles_source.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Reverted this commit.temporarily because load issues.
+# Whenever a manual merge occurs, be sure to check the following commits.
+# Hash: ee8d0b94473df357677cd1f82581251ce0423c01
+# Message: Fix follow suggestions potentially including silenced or blocked accounts (#29306)
+
 class AccountSuggestions::SimilarProfilesSource < AccountSuggestions::Source
   class QueryBuilder < AccountSearchService::QueryBuilder
     def must_clauses
--- spec/models/account_suggestions/source_spec.rb
+++ spec/models/account_suggestions/source_spec.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Reverted this commit.temporarily because load issues.
+# Whenever a manual merge occurs, be sure to check the following commits.
+# Hash: ee8d0b94473df357677cd1f82581251ce0423c01
+# Message: Fix follow suggestions potentially including silenced or blocked accounts (#29306)
+
 require 'rails_helper'
 
 RSpec.describe AccountSuggestions::Source do
