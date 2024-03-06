2271e6d9cb6c945d1b092144eefcf072104b54fa
KMY <tt@kmycode.net>
2024-03-07 08:16:54 +0900

    Revert "Fix follow suggestions potentially including silenced or blocked accounts (#29306)"
    This reverts commit ee8d0b94473df357677cd1f82581251ce0423c01.

--- app/models/account_suggestions/source.rb
+++ app/models/account_suggestions/source.rb
@@ -12,8 +12,6 @@ class AccountSuggestions::Source
   def base_account_scope(account)
     Account
       .searchable
-      .where(discoverable: true)
-      .without_silenced
       .where.not(follows_sql, id: account.id)
       .where.not(follow_requests_sql, id: account.id)
       .not_excluded_by_account(account)
