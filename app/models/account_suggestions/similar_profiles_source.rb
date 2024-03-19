b3c2e47ea89bab4d7b56b8ab26cc108264209cbe
KMY <tt@kmycode.net>
2024-03-19 09:25:17 +0900

    Revertしたコミットを復元

--- app/models/account_suggestions/similar_profiles_source.rb
+++ app/models/account_suggestions/similar_profiles_source.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# Reverted this commit.temporarily because load issues.
-# Whenever a manual merge occurs, be sure to check the following commits.
-# Hash: ee8d0b94473df357677cd1f82581251ce0423c01
-# Message: Fix follow suggestions potentially including silenced or blocked accounts (#29306)
-
 class AccountSuggestions::SimilarProfilesSource < AccountSuggestions::Source
   class QueryBuilder < AccountSearchService::QueryBuilder
     def must_clauses
@@ -56,7 +51,8 @@ class AccountSuggestions::SimilarProfilesSource < AccountSuggestions::Source
     recently_followed_account_ids = account.active_relationships.recent.limit(5).pluck(:target_account_id)
 
     if Chewy.enabled? && !recently_followed_account_ids.empty?
-      QueryBuilder.new(recently_followed_account_ids, account).build.limit(limit).hits.pluck('_id').map(&:to_i).zip([key].cycle)
+      ids_from_es = QueryBuilder.new(recently_followed_account_ids, account).build.limit(limit).hits.pluck('_id').map(&:to_i)
+      base_account_scope(account).where(id: ids_from_es).pluck(:id).zip([key].cycle)
     else
       []
     end
